d0944391cd93eae8ce78cb0368836797333b210c
al <al@fedora.fritz.box>
2025-01-05 08:32:15 +0000

    Package initialisation must come first

--- init.el
+++ init.el
@@ -51,9 +51,9 @@
 (setq straight-use-package-by-default t)
 
 ;; Package archives
+(package-initialize)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
 
 (require 'use-package)
 (setq use-package-always-ensure t)
